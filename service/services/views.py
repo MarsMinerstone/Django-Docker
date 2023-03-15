1b78de717dcac3d867d3b4e259ba07f87c8b41ef
Mars_Minerstone <58552731+MarsMinerstone@users.noreply.github.com>
2023-03-16 02:21:19 +0700

    fourth day
    annotate, agregate

--- service/services/views.py
+++ service/services/views.py
@@ -1,15 +1,30 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from services.models import Subsciprtion
 from services.serializers import SubscriptionSerializer
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from clients.models import Client
+
+from pprint import pprint
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subsciprtion.objects.all().prefetch_related(
-    	'plan',
-        Prefetch('client', queryset=Client.objects.all().select_related('user').only(
-            'company_name',
-            'user__email'))
-    )
-    serializer_class = SubscriptionSerializer
+	queryset = Subsciprtion.objects.all().prefetch_related(
+		'plan',
+		# 'service',
+		Prefetch('client', queryset=Client.objects.all().select_related('user').only(
+			'company_name',
+			'user__email'))
+	).annotate(price=F('service__full_price') - 
+		F('service__full_price') * F('plan__discount_percent') / 100.00) # annotate
+
+	serializer_class = SubscriptionSerializer
+
+	def list(self, request, *args, **kwargs):
+		queryset = self.filter_queryset(self.get_queryset())
+		response = super().list(request, *args, **kwargs)
+
+		# response_data = {'result': response.data} 
+		response.data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+		# response.data = response_data
+
+		return response
